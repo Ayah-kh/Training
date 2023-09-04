065c20b548469d22785783914d50a9ae0c4e7382
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 11:44:09 +0300

    First Commit

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,16 @@
 public class Array {
     public static void main(String[] args) {
+        String[] name=new String[4];
+        name[0]="hussein";
+        name[1]="Ahmad";
+        name[2]="talib";
+        name[3]="alrub";
+
+        System.out.println("name[2] = " + name[2]);
+
+        for (int i = 3; i >=0; i--) {
+            System.out.println(name[i]);
+        }
 
     }
 }
