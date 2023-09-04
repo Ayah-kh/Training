3de3255e6d9ec18dd94dc4d782a7f4309b987cfc
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 11:07:54 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,11 @@ public class Main {
             System.out.println("to school, age: "+age);
         }
         System.out.println("test");
+        System.out.println("=========================");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Welcome ,"+i);
+        }
 
 
     }
