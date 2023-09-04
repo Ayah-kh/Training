0952b1f1d5530936c701c597c1c3c09950eef7d0
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 14:34:45 +0300

    First Commit

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,45 @@
 public class Student {
     String firstName;
     String lastName;
+//    Student(){
+//        System.out.println("class started");
+//    }
+
+    public static void staticMethod(){
+        System.out.println("this is static method");
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     String getStudentInfo(){
         return (firstName+" "+lastName);
     }
 
+
+
     public static void main(String[] args) {
-        Student student=new Student();
-        student.firstName="hussin";
-        student.lastName="ahmad";
+        Student student=new Student("hussin","ahmad");
+
         String studentInfo = student.getStudentInfo();
         System.out.println("studentInfo = " + studentInfo);
+
+        MathStudent mathStudent = new MathStudent("Ayah","Mahmoud");
+        Student.staticMethod();
+        MathStudent.staticMethod();
     }
+}
+
+class MathStudent extends Student{
+
+    public MathStudent(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+        public static void staticMethod(){
+            System.out.println("this is MathStudent static method");
+        }
+
 }
