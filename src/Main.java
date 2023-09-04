5362456938087125fdd48d7c54781fcbc740fba7
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 10:16:04 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int age=26;
+        int oldAge=24;
+        age =oldAge;
+        System.out.println("age = " + age);
     }
 }
