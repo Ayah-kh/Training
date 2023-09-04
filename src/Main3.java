59f57150e009e9156e447ca28a5f6dd311bc6230
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 12:23:13 +0300

    First Commit

--- src/Main3.java
+++ src/Main3.java
@@ -1,20 +1,30 @@
 public class Main3 {
 
-        static void drawLine() {
+    static void drawLine() {
 
-            for (int i = 1; i < 6; i++) {
-                System.out.println("*");
-                for (int j = 0; j < i; j++) {
-                    System.out.print(" ");
-                }
+        for (int i = 1; i < 6; i++) {
+            System.out.println("*");
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
             }
         }
+    }
+
+    static int sum(int n1, int n2) {
+        int r = n1 + n2;
+
+        return r;
+    }
 
     public static void main(String[] args) {
 
-drawLine();
-drawLine();
-drawLine();
+        drawLine();
+        drawLine();
+        drawLine();
+
+        int sum = sum(3, 5);
+        System.out.println("sum = " + sum);
+
 
     }
 }
