e1a1247ec6463b3554f9c90372cbd6c8f3fe3e1d
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-04 10:52:05 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        int n=5;
+        System.out.println("n = " + n);
+        System.out.println("n++ = " + n++);
+        System.out.println("n = " + n);
+        System.out.println("++n = " + ++n);
 
     }
 }
