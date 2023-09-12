2935ff9ddb9a09306d5b96a238d03c9f3c71224c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-12 09:34:34 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
-        int a = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = N; j > i; j--) {
-                a = a + i + j;
-            }
-        }
+        Map<String,String> map=new HashMap<>();
+        map.put(null,"fff");
+        System.out.println("map = " + map);
+        map.put(null,"aaaa");
+        System.out.println("map = " + map);
     }
 }
