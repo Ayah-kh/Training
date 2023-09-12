ba7edc6cbbd2d62a33ac2378f940538d419f1e1c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-12 11:21:32 +0300

    First Commit

---
+++ src/HashTables/Hashtable.java
@@ -0,0 +1,19 @@
+package HashTables;
+
+public class Hashtable {
+    private StudentList[] arr;
+    private int size;
+    private int totalStudent;
+
+    public Hashtable() {
+        size = 1000;
+        arr = new StudentList[size];
+    }
+
+    public int toHashCode(String rollNo) {
+        int addasciis = 0;
+        int codeValue = 0;
+
+    }
+
+}
