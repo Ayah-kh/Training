77bc6153dfb157d9873ff61742da3470b7c65c33
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-18 12:52:34 +0300

    First Commit

--- src/javacollections/ListDemo.java
+++ src/javacollections/ListDemo.java
@@ -15,6 +15,18 @@ public class ListDemo {
 
         System.out.println("fruits = " + fruits);
 
+        List<Integer> first5PrimeNumbers = new ArrayList<>();
+        first5PrimeNumbers.add(2);
+        first5PrimeNumbers.add(3);
+        first5PrimeNumbers.add(5);
+        first5PrimeNumbers.add(7);
+        first5PrimeNumbers.add(11);
+
+        System.out.println("first5PrimeNumbers = " + first5PrimeNumbers);
+
+        List<Integer> first10PrimeNumbers = new ArrayList<>(first5PrimeNumbers);
+        System.out.println("first10PrimeNumbers = " + first10PrimeNumbers);
+
 
     }
 }
