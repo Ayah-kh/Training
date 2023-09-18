136455eb09fdd8a8716594b07685d64a6e975c61
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-18 14:22:45 +0300

    First Commit

--- src/javacollections/ListDemo.java
+++ src/javacollections/ListDemo.java
@@ -13,7 +13,7 @@ public class ListDemo {
         fruits.add("Mango");
         fruits.add("Orange");
 
-        System.out.println("fruits = " + fruits);
+//        System.out.println("fruits = " + fruits);
 
         List<Integer> first5PrimeNumbers = new ArrayList<>();
         first5PrimeNumbers.add(2);
@@ -22,10 +22,10 @@ public class ListDemo {
         first5PrimeNumbers.add(7);
         first5PrimeNumbers.add(11);
 
-        System.out.println("first5PrimeNumbers = " + first5PrimeNumbers);
+//        System.out.println("first5PrimeNumbers = " + first5PrimeNumbers);
 
         List<Integer> first10PrimeNumbers = new ArrayList<>(first5PrimeNumbers);
-        System.out.println("first10PrimeNumbers = " + first10PrimeNumbers);
+//        System.out.println("first10PrimeNumbers = " + first10PrimeNumbers);
 
         List<Integer> next5=new ArrayList<>();
         next5.add(13);
@@ -35,7 +35,25 @@ public class ListDemo {
         next5.add(29);
 
         first10PrimeNumbers.addAll(next5);
-        System.out.println("first10PrimeNumbers = " + first10PrimeNumbers);
+//        System.out.println("first10PrimeNumbers = " + first10PrimeNumbers);
+
+        List<String> programLanguage = new ArrayList<>();
+        System.out.println("programLanguage.isEmpty() = " + programLanguage.isEmpty());
+
+        programLanguage.add("C");
+        programLanguage.add("Java");
+        programLanguage.add("C++");
+        programLanguage.add("Python");
+        programLanguage.add(".net");
+
+        System.out.println("programLanguage.isEmpty() = " + programLanguage.isEmpty());
+        System.out.println("programLanguage.size() = " + programLanguage.size());
+        System.out.println("programLanguage = " + programLanguage);
+
+        String lang = programLanguage.get(1);
+        System.out.println("lang = " + lang);
+
+
 
 
     }
