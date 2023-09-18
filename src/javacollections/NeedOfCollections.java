35b1a8990eb376c14dcb25ebbf10bade298158b3
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-18 12:16:07 +0300

    First Commit

---
+++ src/javacollections/NeedOfCollections.java
@@ -0,0 +1,24 @@
+package javacollections;
+
+import HashTables.Student;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+public class NeedOfCollections {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+
+        Student[] student= new Student[10];
+        student[0]=new Student("Hal","455",2.2);
+
+
+        Map<String,String> map= new Hashtable<>();
+        List<String> list;
+
+    }
+}
