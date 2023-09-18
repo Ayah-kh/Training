5506ddbdebd2405097aebb3ecc3c2e2cddd1f619
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-18 17:41:09 +0300

    First Commit

--- src/javacollections/LinkedListEx.java
+++ src/javacollections/LinkedListEx.java
@@ -14,5 +14,29 @@ public class LinkedListEx {
         System.out.println("fruits = " + fruits);
         fruits.add(2,"watermelon");
         System.out.println("fruits = " + fruits);
+
+        fruits.addFirst("Strawberry");
+        System.out.println("fruits = " + fruits);
+
+        fruits.addLast("orange");
+        System.out.println("fruits = " + fruits);
+        System.out.println("============================");
+        String first = fruits.getFirst();
+        String last = fruits.getLast();
+        System.out.println("first = " + first);
+        System.out.println("last = " + last);
+        System.out.println("fruits = " + fruits);
+        String s = fruits.get(3);
+        System.out.println("s = " + s);
+        System.out.println("============================");
+        String s1 = fruits.removeFirst();
+        String s2 = fruits.removeLast();
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        System.out.println("fruits = " + fruits);
+        String s3 = fruits.remove(1);
+        System.out.println("s3 = " + s3);
+        System.out.println("fruits = " + fruits);
+
     }
 }
