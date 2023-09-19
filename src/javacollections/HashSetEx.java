52ee372920288fc0c38733c04a577bb9ab41f617
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-19 16:04:39 +0300

    First Commit

--- src/javacollections/HashSetEx.java
+++ src/javacollections/HashSetEx.java
@@ -1,8 +1,12 @@
 package javacollections;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class HashSetEx {
     public static void main(String[] args) {
@@ -16,5 +20,14 @@ public class HashSetEx {
         System.out.println("languages = " + languages);
         languages.add("Java");
         System.out.println("languages = " + languages);
+
+        int[] arr= {11,12,12,8,963,74,56,20,33,66,54,23,13,14,20};
+        Set<Integer> collect = IntStream.of(arr).boxed()
+                .collect(Collectors.toSet());
+        System.out.println("collect = " + collect);
+
+//        Set<Integer> integers=new HashSet<>(Arrays.asList(arr));
+//        System.out.println("integers = " + integers);
+
     }
 }
