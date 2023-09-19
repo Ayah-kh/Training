5c2961154b0c9ac34f764118ad994092315dbc5e
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-19 18:11:05 +0300

    First Commit

--- src/javacollections/HashMapEx.java
+++ src/javacollections/HashMapEx.java
@@ -1,17 +1,36 @@
 package javacollections;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapEx {
     public static void main(String[] args) {
-        Map<String,Integer> numberMApping = new HashMap<>();
+        Map<String,Integer> numberMapping = new HashMap<>();
 
-        numberMApping.put("One",1);
-        numberMApping.put("Two",2);
-        numberMApping.put("Three",3);
-        numberMApping.put("Four",4);
-        numberMApping.put("Five",5);
+        numberMapping.put("One",1);
+        numberMapping.put("Two",2);
+        numberMapping.put("Three",3);
+        numberMapping.put("Four",4);
+        numberMapping.put("Five",5);
+
+        System.out.println("numberMapping = " + numberMapping);
+
+        System.out.println("numberMapping.containsKey(\"Four\") = " + numberMapping.containsKey("Four"));
+        System.out.println("numberMapping.containsValue(3) = " + numberMapping.containsValue(3));
+
+        for (Map.Entry<String,Integer> entry: numberMapping.entrySet()){
+            System.out.println("Key: "+entry.getKey()+
+                    ", value: "+entry.getKey());
+        }
+
+        Set<Map.Entry<String, Integer>> entries = numberMapping.entrySet();
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println(entry);
+        }
+
+        numberMapping.forEach((K,V)-> System.out.println(K+" K:V "+V));
 
 
     }
