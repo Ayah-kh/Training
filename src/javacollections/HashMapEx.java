13f19bcd3395d58a3525a54b9dd5e20b626dfe9b
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-19 16:51:01 +0300

    First Commit

--- src/javacollections/HashMapEx.java
+++ src/javacollections/HashMapEx.java
@@ -1,5 +1,18 @@
 package javacollections;
 
-public class HashMapEx {
+import java.util.HashMap;
+import java.util.Map;
 
+public class HashMapEx {
+    public static void main(String[] args) {
+        Map<String,Integer> numberMApping = new HashMap<>();
+
+        numberMApping.put("One",1);
+        numberMApping.put("Two",2);
+        numberMApping.put("Three",3);
+        numberMApping.put("Four",4);
+        numberMApping.put("Five",5);
+
+
+    }
 }
