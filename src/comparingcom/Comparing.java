019cdabc2dcc47b2991aa1093af44517344efc0d
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-20 15:00:34 +0300

    First Commit

---
+++ src/comparingcom/Comparing.java
@@ -0,0 +1,15 @@
+package comparingcom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comparing {
+    public static void main(String[] args) {
+        List<Laptop> laptops = new ArrayList<>();
+        laptops.add(new Laptop("Dell",16,800));
+        laptops.add(new Laptop("Apple",8,1200));
+        laptops.add(new Laptop("Acer",12,700));
+
+
+    }
+}
