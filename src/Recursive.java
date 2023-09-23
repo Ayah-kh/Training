39bbf098c251aa06f2b3541388b8a5f0aa95eef3
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-23 18:36:25 +0300

    First Commit

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,19 +1,22 @@
 public class Recursive {
 
-    public boolean isPalindrome(int num){
-        String s = String.valueOf(num);
-        return isPalindrome(s);
+//    public boolean isPalindrome(int num){
+//        String s = String.valueOf(num);
+//        return isPalindrome(s);
+//
+//    }
 
-    }
-
-    public boolean isPalindrome(String string) {
-        if (string.equals("")||string.length()==1)
-            return true;
-        if (string.indent())
-        return false;
-    }
+//    public boolean isPalindrome(String string) {
+//        if (string.equals("")||string.length()==1)
+//            return true;
+//        if (string.indent())
+//        return false;
+//    }
 
     public static void main(String[] args) {
+    String s= "abcde";
+
+
 
     }
 }
