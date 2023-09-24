cb4d028f78dd47fe91348152de2a27264ddfa0b4
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-24 15:17:14 +0300

    First Commit

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,8 +1,58 @@
+import java.util.Arrays;
+
 public class Recursive {
 
     public static void main(String[] args) {
-        int s=1233221;
-        System.out.println("isPalindrome(s) = " + isPalindrome(s));
+        char[] arr= {'a','b','c','d'};
+        printString(arr,2);
+    }
+
+    public static void printNum(int [] arr, int k){
+        int n = arr.length;
+        int[] newArray= new int[k];
+            printNum(arr,k,n,newArray);
+
+    }
+    private static void printNum(int [] arr, int k,int n,int[] newArray) {
+        if (newArray.length==k) {
+            System.out.println("Arrays.toString(newArray) = " + Arrays.toString(newArray));
+        }
+
+
+
+    }
+
+
+    public static void  printString(char[] chars, int k){
+        int n= chars.length;
+        printString(chars,k,n,"");
+    }
+
+    private static void printString(char[] chars, int k, int n, String prefix) {
+        if (k==0) {
+            System.out.println(prefix);
+            return;
+        }
+
+        if (n==0){
+            System.out.println("");
+            return;
+        }
+
+
+
+        System.out.println("method called ===========");
+        System.out.println("n = " + n);
+        System.out.println("k = " + k);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("inside loop, i="+i);
+            String newPreficx= prefix+chars[i];
+            System.out.println("newPreficx = " + newPreficx);
+            printString(chars,k-1,n,newPreficx);
+
+        }
+
 
     }
 
