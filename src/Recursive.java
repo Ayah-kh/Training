442a1ae3d48c5c1fc12aa53a4f2bcd3941ba3562
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-09-24 16:09:35 +0300

    First Commit

--- src/Recursive.java
+++ src/Recursive.java
@@ -3,29 +3,35 @@ import java.util.Arrays;
 public class Recursive {
 
     public static void main(String[] args) {
-        char[] arr= {'a','b','c','d'};
-        printString(arr,2);
+//        char[] arr= {'a','b','c','d'};
+        int [] arr2={1,2,3,4,5};
+        printNum(arr2,3);
     }
 
     public static void printNum(int [] arr, int k){
         int n = arr.length;
         int[] newArray= new int[k];
-        int loopStart=0;
-            printNum(arr,k,n,loopStart,newArray);
+        int i  =0;
+            printNum(arr,k,n,i,"");
 
     }
-    private static void printNum(int [] arr, int k,int n,int i, int[] newArray) {
-        if (newArray.length==k) {
-            System.out.println("Arrays.toString(newArray) = " + Arrays.toString(newArray));
+    private static void printNum(int [] arr, int k,int n,int i, String startPoint) {
+        if (k==0) {
+            System.out.println(startPoint);
             return;
         }
 
+        System.out.println("method called ===========");
+        System.out.println("n = " + n);
+        System.out.println("k = " + k);
+
         for ( int j=i; j <n ; j++) {
-            printNum(arr,k,n,i+1,newArray);
+            System.out.println("j = " + j);
+            System.out.println("i = " + i);
+            String startPoint2=startPoint+ arr[i];
+            System.out.println("startPoint2 = " + startPoint2);
+            printNum(arr,k-1,n,i+1,startPoint2);
         }
-
-
-
 
     }
 
@@ -38,11 +44,6 @@ public class Recursive {
     private static void printString(char[] chars, int k, int n, String prefix) {
         if (k==0) {
             System.out.println(prefix);
-            return;
-        }
-
-        if (n==0){
-            System.out.println("");
             return;
         }
 
