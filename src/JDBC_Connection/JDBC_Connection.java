003e1a9873844a3f410778ea5e544ace7266246f
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-04 10:55:33 +0300

    First Commit

--- src/JDBC_Connection/JDBC_Connection.java
+++ src/JDBC_Connection/JDBC_Connection.java
@@ -9,10 +9,11 @@ import java.sql.Statement;
 public class JDBC_Connection {
     public static void main(String[] args) throws SQLException{
 
+        String url ="jdbc:mysql://localhost:3306/university";
         String userName = "root";
         String password = "root";
         String query = "select * from engineeringstudent";
-        String url ="jdbc:mysql://localhost:3306/university";
+
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -34,10 +35,15 @@ public class JDBC_Connection {
                 System.out.println(UniversityData);
             }
 
+            result.close();
+            statement.close();
+            connection.close();
 
         }catch (SQLException e){
             e.printStackTrace();
         }
+
+
 
     }
 }
