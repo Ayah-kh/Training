732ffeb388c9e28a37652d7ce28f885e96e9481c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-09 10:29:37 +0300

    First Commit

--- src/File/FileTest2.java
+++ src/File/FileTest2.java
@@ -1,17 +1,31 @@
 package File;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileTest2 {
     public static void main(String[] args) {
+//        try {
+//            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\DELL\\Desktop\\Test\\file1.txt"));
+//            bw.write("KAren\n");
+//            bw.write("shad\n");
+//            bw.write("sam\n");
+//            bw.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\DELL\\Desktop\\Test\\file1.txt"));
-            bw.write("KAren\n");
-            bw.write("shad\n");
-            bw.write("sam\n");
-            bw.close();
+            BufferedReader bufferedReader =
+                    new BufferedReader(
+                            new FileReader("C:\\Users\\DELL\\Desktop\\Test\\file1.txt"));
+            String s;
+            while ((s=bufferedReader.readLine())!=null){
+                System.out.println(s);
+
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
