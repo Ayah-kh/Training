0f3d2c9e6da586d20eeee0fe089a6c2acbbe7f54
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-09 09:59:53 +0300

    First Commit

--- src/File/FileTest.java
+++ src/File/FileTest.java
@@ -1,7 +1,10 @@
 package File;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileTest {
     public static void main(String[] args) {
@@ -14,6 +17,32 @@ public class FileTest {
             }
         }catch (IOException e){
             System.out.println("error");
+            e.printStackTrace();
+        }
+
+        System.out.println("myFile.getAbsolutePath() = " + myFile.getAbsolutePath());
+
+
+        try {
+            FileWriter writer = new FileWriter("filename.txt");
+            writer.write("Files in java\n");
+            writer.write("Files in java");
+            writer.write("Files in java");
+            writer.write("Files in java");
+            writer.close();
+            System.out.println("Success");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("test");
+
+        try {
+            Scanner reader = new Scanner(myFile);
+            while (reader.hasNext()){
+                System.out.println(reader.nextLine());
+            }
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
