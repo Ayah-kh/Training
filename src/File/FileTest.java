0136aeae05a88d88f23f96941b4f39def02fc3b5
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-09 09:18:41 +0300

    First Commit

--- src/File/FileTest.java
+++ src/File/FileTest.java
@@ -1,10 +1,22 @@
 package File;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileTest {
     public static void main(String[] args) {
         File myFile = new File("filename.txt");
+        try {
+            if (myFile.createNewFile()){
+                System.out.println("File created: "+myFile.getName());
+            }else {
+                System.out.println("File already exists");
+            }
+        }catch (IOException e){
+            System.out.println("error");
+            e.printStackTrace();
+        }
+
 
     }
 }
