d11f03d2678373a2a6cce99f12525b05fb3f0300
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-10 12:28:14 +0300

    First Commit

---
+++ src/File/ReadFile.java
@@ -0,0 +1,22 @@
+package File;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadFile {
+  public static void main(String[] args) {
+    try {
+      File file = new File("filename.txt");
+      Scanner myReader = new Scanner(file);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
