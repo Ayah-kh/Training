85bf2f64fda5cb3e5914cb7f81fd55e499839860
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-10 14:04:47 +0300

    First Commit

--- src/DesignPatterns/singleton/LazySingleton.java
+++ src/DesignPatterns/singleton/LazySingleton.java
@@ -5,11 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class LazySingleton {
+
     private String[] letters = {"a","b","c","d","e"};
 
     private List<String> data = Arrays.asList(letters);
 
-    public LazySingleton (){
+    private static LazySingleton instance;
+
+    public static LazySingleton getInstance(){
+        if (instance == null)
+            instance=new LazySingleton();
+        return instance;
+    }
+
+    private LazySingleton (){
         Collections.shuffle(data);
     }
 
--- src/DesignPatterns/singleton/Main.java
+++ src/DesignPatterns/singleton/Main.java
@@ -2,10 +2,13 @@ package DesignPatterns.singleton;
 
 public class Main {
     public static void main(String[] args) {
-        LazySingleton inst1= new LazySingleton();
-        LazySingleton inst2= new LazySingleton();
+        LazySingleton inst1=LazySingleton.getInstance();
+        LazySingleton inst2= LazySingleton.getInstance();
 
+        System.out.println("inst1.hashCode() = " + inst1.hashCode());
         inst1.printData();
+
+        System.out.println("inst2.hashCode() = " + inst2.hashCode());
         inst2.printData();
     }
 }
