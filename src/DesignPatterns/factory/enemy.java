0fe149fb6e62128d732cc18f303fa8ab17c4e8c2
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-10 12:33:39 +0300

    First Commit

---
+++ src/DesignPatterns/factory/enemy.java
@@ -0,0 +1,37 @@
+package DesignPatterns.factory;
+
+public abstract class enemy {
+    private String name;
+    private int damage;
+    private int health;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void showUp(){
+        System.out.printf(
+                "[%s] is showing up, Damage is [%d],Health is [%d]"
+                ,getName(),getDamage(),getHealth());
+    }
+}
