adb0b4b7f015df5e5c9dbc6f15ce7c3e14b09fa5
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-11 15:06:30 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = Arrays.copyOf(new int[]{1, 2, 3}, 5);
+        double sqrt = Math.sqrt(5);
+    }
+}
