1abbe74162d8609be4e57222fc934d48da4c8316
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-14 12:28:06 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import DesignPatterns.factory2.MotorCycleFactory;
+import DesignPatterns.factory2.Motorcycle;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] intArray = Arrays.copyOf(new int[]{1, 2, 3}, 5);
-        double sqrt = Math.sqrt(5);
+
     }
 }
