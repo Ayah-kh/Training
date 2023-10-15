9dda829e6132c43327fff2f4327a0d77cd9621eb
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-15 13:23:20 +0300

    First Commit

---
+++ src/builder2/ChickenBurger.java
@@ -0,0 +1,13 @@
+package builder2;
+
+public class ChickenBurger extends Burger {
+    @Override
+    public String name() {
+        return "Chicken Burger";
+    }
+
+    @Override
+    public float price() {
+        return 50.5f;
+    }
+}
