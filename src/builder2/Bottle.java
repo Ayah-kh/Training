45f3a261ad1cd959aa53da186cf739193a60d52a
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-15 13:16:39 +0300

    First Commit

---
+++ src/builder2/Bottle.java
@@ -0,0 +1,8 @@
+package builder2;
+
+public class Bottle implements Packing{
+    @Override
+    public String pack() {
+        return "Bottle";
+    }
+}
---
+++ src/builder2/Wrapper.java
@@ -0,0 +1,8 @@
+package builder2;
+
+public class Wrapper implements Packing{
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+}
