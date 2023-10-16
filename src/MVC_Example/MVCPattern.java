9660afe94242e127f41bc1d9b24621e3079b4c2d
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-16 12:37:20 +0300

    First Commit

--- src/MVC_Example/MVCPattern.java
+++ src/MVC_Example/MVCPattern.java
@@ -3,6 +3,11 @@ package MVC_Example;
 public class MVCPattern {
     public static void main(String[] args) {
         Student model = retriveStudentFromDatabase();
+        StudentView view= new StudentView();
+        StudentController controller= new StudentController(model,view);
+        controller.updateView();
+        controller.setStudentName("Vikram sharma");
+        controller.updateView();
 
     }
 
